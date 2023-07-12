25f192cd374b91aabbe19f2aa33d1fd6e4bfdd4f
nthimoni <thimoniern@gmail.com>
2023-07-12 18:03:21 +0200

    add Client class (in progress)

---
+++ include/Client.hpp
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Client.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nthimoni <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/11 15:22:15 by nthimoni          #+#    #+#             */
+/*   Updated: 2023/07/12 17:38:24 by nthimoni         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLIENT_HPP
+#define CLIENT_HPP
+
+#include <string>
+#include <vector>
+
+#include "PacketBuffer.hpp"
+
+class Channel;
+
+class Client
+{
+  public:
+	Client();
+	Client(const Client& other);
+	Client& operator=(const Client& rhs);
+
+	~Client();
+
+	const std::string& getUsername() const;
+	const std::string& getNickname() const;
+	void setUsername(const char* username);
+	void setNickanme(const char* nickname);
+	void joinChannel(const char* name);
+	void leaveChannel(const char* name);
+
+  private:
+	std::string m_nickname;
+	std::string m_username;
+	PacketBuffer buf;
+	std::vector<Channel*> channels;
+};
+
+#endif
---
+++ src/Client.cpp
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Client.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nthimoni <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/11 18:15:47 by nthimoni          #+#    #+#             */
+/*   Updated: 2023/07/12 15:27:12 by nthimoni         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Client.hpp"
+
+Client::Client() {}
+
+Client::Client(const Client& other)
+	: m_nickname(other.m_nickname), m_username(other.m_username), buf(other.buf), channels(other.channels)
+{
+}
+
+Client& Client::operator=(const Client& rhs)
+{
+	if (this != &rhs)
+	{
+		m_nickname = rhs.m_nickname;
+		m_username = rhs.m_username;
+		buf = rhs.buf;
+		channels = rhs.channels;
+	}
+
+	return *this;
+}
+
+Client::~Client() {}
+
+const std::string& Client::getUsername() const
+{
+	return m_username;
+}
+
+const std::string& Client::getNickname() const
+{
+	return m_nickname;
+}
+
+void Client::setUsername(const char* username)
+{
+	m_username = username;
+}
+
+void Client::setNickanme(const char* nickname)
+{
+	m_nickname = nickname;
+}
