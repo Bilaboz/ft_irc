42ea569fbc88ac8404821b83794d5ee3e4cb7f77
nthimoni <thimoniern@gmail.com>
2023-07-18 14:45:10 +0200

    Format server.cpp

--- src/Server.cpp
+++ src/Server.cpp
@@ -6,18 +6,18 @@
 /*   By: nthimoni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/10 20:21:58 by nthimoni          #+#    #+#             */
-/*   Updated: 2023/07/12 16:01:25 by nthimoni         ###   ########.fr       */
+/*   Updated: 2023/07/18 14:44:50 by nthimoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Server.hpp"
 
-Server::Server(int port) : m_port(port)
-{
-	
-}
+Server::Server(int port) : m_port(port) {}
 
-int Server::poll() {}
+int Server::poll()
+{
+	return 1;
+}
 
 void Server::stop() {}
 
@@ -25,6 +25,13 @@ Server::~Server() {}
 
 Server::Server() : m_port(0) {}
 
-Server::Server(const Server& other) : m_port(0) {}
+Server::Server(const Server& other) : m_port(0)
+{
+	(void)other;
+}
 
-Server& Server::operator=(const Server& rhs) {}
+Server& Server::operator=(const Server& rhs)
+{
+	(void)rhs;
+	return *this;
+}
