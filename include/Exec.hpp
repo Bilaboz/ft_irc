89ff95e2d08439286b9afb32d1944cff5e7d8a1c
Bilaboz <bilaboz@protonmail.com>
2023-07-19 18:15:40 +0200

    create RPL.hpp

--- include/Exec.hpp
+++ include/Exec.hpp
@@ -6,7 +6,7 @@
 /*   By: nthimoni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/19 14:30:38 by nthimoni          #+#    #+#             */
-/*   Updated: 2023/07/19 16:39:05 by rcarles          ###   ########.fr       */
+/*   Updated: 2023/07/19 17:45:10 by rcarles          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,13 +24,13 @@
 class Exec
 {
   public:
+	typedef int (*func)(const Message&, ClientsManager&, int, std::vector<Channel>&);
+	typedef std::vector<Channel>::iterator ChannelsIt;
+
 	static int exec(
 		const Message& message, ClientsManager& clients, int fd,
 		std::vector<Channel>& channels
 	);
-
-	typedef int (*func)(const Message&, ClientsManager&, int, std::vector<Channel>&);
-	typedef std::vector<Channel>::iterator ChannelsIt;
 
   private:
 	static const std::map<std::string, func> m_functions;
---
+++ include/RPL.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   RPL.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rcarles <rcarles@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/19 17:54:41 by rcarles           #+#    #+#             */
+/*   Updated: 2023/07/19 18:14:00 by rcarles          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef RPL_HPP
+# define RPL_HPP
+
+#define RPL_WELCOME "001"
+#define RPL_NOTOPIC "331"
+#define RPL_TOPIC "332"
+#define RPL_TOPICWHOTIME "333"
+#define ERR_NOSUCHCHANNEL "403"
+#define ERR_NEEDMOREPARAMS "461"
+#define ERR_CHANOPRIVSNEEDED "482"
+
+
+#endif
